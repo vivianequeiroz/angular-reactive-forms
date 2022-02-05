ae546acdbfb3da5f9c954aa70dc371243faab6a8
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-02-05 18:37:18 -0300

    fix: ad ReactiveFormsModule into app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customers/customer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CustomerComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, CustomerComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
